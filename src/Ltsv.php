a5f0749ccc5edacc06be960fc62fc264b1cf6ced
ksomemo <kntsmy@hotmail.co.jp>
2013-03-16 17:40:52 +0900

    test

--- src/Ltsv.php
+++ src/Ltsv.php
@@ -14,8 +14,34 @@ class Ltsv
 
 	public function set($key, $value)
 	{
-		$this->hash[$key] = $value;
+		$beforeValue = null;
 
-		return null;
+		if ($key === null || $key === '') {
+			throw new InvalidArgumentException();
+		}
+
+		if ($value === null) {
+			throw new InvalidArgumentException();
+		}
+
+		if (isset($this->hash[$key])) {
+			$beforeValue = $this->hash[$key];
+
+			unset($this->hash[$key]);
+		}
+
+		$this->hash[$key] = str_replace(array(':', "¥n", "¥t"), array("¥¥:", "¥¥n", "¥¥t"), $value);
+
+		return $beforeValue;
+	}
+
+	public function dump()
+	{
+		$return = '';
+		foreach($this->hash as $key => $value) {
+			$return .= $key . ':' . $value . "¥t";
+		}
+
+		return preg_replace("/¥t$/", "¥n", $return);
 	}
 }
--- test/LtsvTest.php
+++ test/LtsvTest.php
@@ -3,25 +3,140 @@ require_once '../src/Ltsv.php';
 
 class LtsvTest extends PHPUnit_Framework_TestCase
 {
+	protected $t;
+
+	public function setUp()
+	{
+		$this->t = new Ltsv();
+	}
+
 	public function test存在しないキーを呼び出したときにnullを返す()
 	{
-		$t = new Ltsv();
-
-		$this->assertNull($t->get('foo'));
+		$this->assertNull($this->t->get('foo'));
 	}
 
 	public function test存在するキーを呼び出したときに格納されている値を返す()
 	{
-		$t = new Ltsv();
-		$t->set('foo', 'bar');
+		$this->t->set('foo', 'bar');
 
-		$this->assertEquals('bar', $t->get('foo'));
+		$this->assertEquals('bar', $this->t->get('foo'));
 	}
 
-	public function testセットしたときにnullを返すこと()
+	public function testセットしたときにnullを返す()
 	{
-		$t = new Ltsv();
-		$actual = $t->set('foo', 'bar');
+		$actual = $this->t->set('foo', 'bar');
+
 		$this->assertNull($actual);
+	}
+
+	public function test何も格納されていないときdumpすると空文字列を返す()
+	{
+		$actual = $this->t->dump();
+
+		$this->assertEquals('', $actual);
+	}
+
+	public function test1件格納されているときにdumpするとキーバリューを返す()
+	{
+		$this->t->set('foo', 'hoge');
+		$actual = $this->t->dump();
+
+		$this->assertEquals("foo:hoge¥n", $actual);
+	}
+
+	public function test2件格納されているときにdumpするとタブ区切りでキーバリューを返す()
+	{
+		$this->t->set('bar', 'fuga');
+		$this->t->set('foo', 'piyo');
+		$actual = $this->t->dump();
+
+		$this->assertEquals("bar:fuga¥tfoo:piyo¥n", $actual);
+	}
+
+	public function test既に入っている値をリセットすると置き換える以前の値を返す()
+	{
+		$this->t->set('foo', 'fuga');
+		$actual = $this->t->set('foo', 'piyo');
+
+		$this->assertEquals("fuga", $actual);
+	}
+
+	public function test2件格納されているときに、１件目をリセットしてdumpすると格納順にキーバリューを返す()
+	{
+		$this->t->set('foo', 'hoge');
+		$this->t->set('bar', 'fuga');
+		$this->t->set('foo', 'piyo');
+		$actual = $this->t->dump();
+
+		$this->assertEquals("bar:fuga¥tfoo:piyo¥n", $actual);
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testキーをnullにして格納するとき例外を投げる()
+	{
+		$this->t->set(null, 'hoge');
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testキーが空文字列のとき例外を投げる()
+	{
+		$this->t->set('', 'foo');
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testNull値を格納するとき例外を投げる()
+	{
+		$this->t->set('foo', null);
+	}
+
+	public function test空文字列を格納できる()
+	{
+		$this->t->set('foo', '');
+		$actual = $this->t->get('foo');
+
+		$this->assertEquals('', $actual);
+	}
+
+	public function test1件格納して値が空文字列のときdumpしても空文字列である()
+	{
+		$this->t->set('foo', '');
+		$actual = $this->t->dump();
+
+		$this->assertEquals("foo:¥n", $actual);
+	}
+
+	public function test2件格納して値が空文字列のときdumpしても空文字列である()
+	{
+		$this->t->set('foo', '');
+		$this->t->set('bar', 'fuga');
+		$actual = $this->t->dump();
+
+		$this->assertEquals("foo:¥tbar:fuga¥n", $actual);
+	}
+
+	/**
+	 * @dataProvider escapeTargetList
+	 */
+	public function testエスケープ対象の文字列($key, $value, $expected)
+	{
+		$this->t->set($key, $value);
+		$actual = $this->t->get($key);
+
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function escapeTargetList()
+	{
+		return array(
+			array('foo', ':', "¥¥:"),
+			array('foo', "¥n", "¥¥n"),
+			array('foo', "¥t", "¥¥t")
+		);
 	}
 }
